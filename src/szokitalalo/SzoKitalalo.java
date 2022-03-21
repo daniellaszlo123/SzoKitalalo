5aec69f0d206474cb620db5055cfc72f1c3b74b1
kovacsdaniel422 <kovacsdaniel422@gmail.com>
2022-03-21 15:34:32 +0100

    Update SzoKitalalo.java

--- src/szokitalalo/SzoKitalalo.java
+++ src/szokitalalo/SzoKitalalo.java
@@ -1,18 +1,19 @@
 package szokitalalo;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class SzoKitalalo {
 
     static String[] szavak = {"fa", "ág", "tő"};
     Random rnd = new Random();
-
+    Scanner bead = new Scanner(System.in);
     private static void valaszt() {
 
     }
 
     private static void beker() {
-
+        System.out.print("Kérem adjon meg 2 betűt: ");
     }
 
     private static void kiirat() {
