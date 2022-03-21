791a004a28100d7ac21e0c480223fc6e94d5c497
kovacsdaniel422 <kovacsdaniel422@gmail.com>
2022-03-21 15:29:40 +0100

    Kiinduló

---
+++ src/szokitalalo/SzoKitalalo.java
@@ -0,0 +1,28 @@
+package szokitalalo;
+
+import java.util.Random;
+
+public class SzoKitalalo {
+
+    static String[] szavak = {"fa", "ág", "tő"};
+    Random rnd = new Random();
+
+    private static void valaszt() {
+
+    }
+
+    private static void beker() {
+
+    }
+
+    private static void kiirat() {
+        
+    }
+
+    public static void main(String[] args) {
+        valaszt();
+        beker();
+        kiirat();
+    }
+
+}
