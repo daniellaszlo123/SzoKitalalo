3e5866083b7f2199ab139f1188008f648ad5fc80
kovacsdaniel422 <kovacsdaniel422@gmail.com>
2022-03-24 23:40:07 +0100

    Update SzoKitalalo.java

--- src/szokitalalo/SzoKitalalo.java
+++ src/szokitalalo/SzoKitalalo.java
@@ -5,57 +5,74 @@ import java.util.Scanner;
 
 public class SzoKitalalo {
 
-    static String[] szavak = {"fa", "ág", "tó"};
+    static String[] szavak = { "fa", "ág", "tó" };
     static Random rnd = new Random();
     public static Scanner scanner = new Scanner(System.in);
 
     private static String valaszt() {
         int egySzo = rnd.nextInt(3);
-    return szavak[egySzo];
+        System.out.println(egySzo);
+        return szavak[egySzo];
     }
 
     private static String beker() {
         String szoBe;
         System.out.println("Kérem adjon meg egy két betű szót: ");
         szoBe = scanner.nextLine();
-//        System.out.print(szoBe);
-        if(szoBe.length()>2 || szoBe.length()<2){
-            while (!(szoBe.length()==2)){
-                System.out.println("Nem két betűs szót adott meg!Újra: ");
+        // System.out.print(szoBe);
+        if (szoBe.length() != 2) {
+            while (!(szoBe.length() == 2)) {
+                System.out.println("Nem két betűs szót adott meg! Újra: ");
                 szoBe = scanner.nextLine();
             }
         }
-        
+
         return szoBe;
     }
-    
-    private static void dontes(){
-//        char[] szavunk = new char[valaszt().length()];
-//        char[] tipp = new char[beker().length()];
+
+    private static void dontes() {
+        // char[] szavunk = new char[valaszt().length()];
+        // char[] tipp = new char[beker().length()];
         String szavunk = valaszt();
-        String tipp = beker();
-        char elsoT=tipp.charAt(0);
-        char masodikT=tipp.charAt(1);
-        char elsoSz=szavunk.charAt(0);
-        char masodikSz=szavunk.charAt(1);
-        if(elsoT==elsoSz || elsoT==masodikSz){
-            System.out.print("'"+elsoT+"'"+" betű benne van.");
-        }else if(masodikT==elsoSz || masodikT==masodikSz){
-            System.out.print("'"+masodikT+"'"+" betű benne van.");
-        }else{
-            System.out.println("Egyik betű sem szerepel a szóban.");
+        while (true) {
+
+            String tipp = beker();
+            char elsoT = tipp.charAt(0);
+            char masodikT = tipp.charAt(1);
+            char elsoSz = szavunk.charAt(0);
+            char masodikSz = szavunk.charAt(1);
+            if (elsoT == elsoSz && masodikT == masodikSz) {
+                System.out.println("Kitaláltad a szót! Gratulálok!");
+                break;
+            } else if (elsoT == elsoSz || elsoT == masodikSz) {
+                System.out.println("'" + elsoT + "'" + " betű benne van.");
+            } else if (masodikT == elsoSz || masodikT == masodikSz) {
+                System.out.println("'" + masodikT + "'" + " betű benne van.");
+            } else {
+                System.out.println("Egyik betű sem szerepel a szóban.");
+            }
         }
 
     }
 
-    private static void kiirat() {
-        
+    private static void newGame() {
+        System.out.println("Szeretnél játstszani még egy kört? (I/N)");
+        String awn = scanner.nextLine();
+        while (awn == "I" && awn == "i" && awn == "N" && awn == "n") {
+            System.out.println("Szeretnél játstszani még egy kört? (I/N)");
+            awn = scanner.nextLine();
+        }
+        if (awn == "I" || awn == "i") {
+            dontes();
+        } else {
+            System.out.println("Köszönjük hogy játszottál");
+        }
     }
 
     public static void main(String[] args) {
-        String kitalalando=valaszt();
+        // String kitalalando = valaszt();
         dontes();
-        kiirat();
+        newGame();
     }
 
 }
