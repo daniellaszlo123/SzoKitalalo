553ab4dbaba1614d0ccdf0069af864025789cfe2
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-03-25 09:07:40 +0100

    public static Scanner javítása, ékezetes szavak nem jók, felesleges if eltávolítása, végtelen ciklus eltávolítása, newGame() javítása, kódismétlés javítása

--- src/szokitalalo/SzoKitalalo.java
+++ src/szokitalalo/SzoKitalalo.java
@@ -5,75 +5,93 @@ import java.util.Scanner;
 
 public class SzoKitalalo {
 
-    static String[] szavak = { "fa", "ág", "tó" };
+    static String[] szavak = { "fa", "ag", "to" };//{ "fa", "ág", "tó" }; ékezeteket nem találja
+    /*DEBUG
+    tó
+    Kérem adjon meg egy két betűs szót: tó
+
+    't' betű benne van, és jó helyen.
+    Nincs benne a második karakter.
+    */
+
     static Random rnd = new Random();
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in); //public static nem jó
+    static int szamlalo=0;
 
     private static String valaszt() {
         int egySzo = rnd.nextInt(3);
-        //System.out.println(egySzo);
         return szavak[egySzo];
     }
 
     private static String beker() {
         String szoBe;
-        System.out.print("Kérem adjon meg egy két betű szót: ");
+        System.out.print("Kérem adjon meg egy két betűs szót: ");
         szoBe = scanner.nextLine();
         System.out.println("");
-//        System.out.print(szoBe);
-        if (szoBe.length() != 2) {
-            while (!(szoBe.length() == 2)) {
-                System.out.println("Nem két betűs szót adott meg! Újra: ");
-                szoBe = scanner.nextLine();
-            }
+        while (!(szoBe.length() == 2)) { //if felesleges
+            System.out.println("Nem két betűs szót adott meg! Újra: ");
+            szoBe = scanner.nextLine();
+            System.out.println("");
         }
 
         return szoBe;
     }
 
     private static void dontes() {
-        // char[] szavunk = new char[valaszt().length()];
-        // char[] tipp = new char[beker().length()];
         String szavunk = valaszt();
-        while (true) {
-
-            String tipp = beker();
-            char elsoT = tipp.charAt(0);
-            char masodikT = tipp.charAt(1);
-            char elsoSz = szavunk.charAt(0);
-            char masodikSz = szavunk.charAt(1);
-            if (elsoT == elsoSz && masodikT == masodikSz) {
-                System.out.println("Kitaláltad a szót! Gratulálok!");
-                break;
-            } else if (elsoT == elsoSz || elsoT == masodikSz) {
-                System.out.println("'" + elsoT + "'" + " betű benne van.");
-            } else if (masodikT == elsoSz || masodikT == masodikSz) {
-                System.out.println("'" + masodikT + "'" + " betű benne van.");
-            } else {
-                System.out.println("Egyik betű sem szerepel a szóban.");
-            }
+        System.out.println(szavunk);
+        String tipp = beker();
+        char elsoT = tipp.charAt(0);
+        char masodikT = tipp.charAt(1);
+        char elsoSz = szavunk.charAt(0);
+        char masodikSz = szavunk.charAt(1);
+        String ki="";
+        while (!(elsoT == elsoSz && masodikT == masodikSz)) { //végtelen ciklus kijavítás, break-et nem használunk
+            boolean elsoElsovel=elsoT==elsoSz, elsoMasodikkal=elsoT==masodikSz, 
+                masodikElsovel=masodikT==elsoSz, masodikMasodikkal=masodikT==masodikSz;
+            szamlalo++;
+            ki=melyikVanJoHelyen(elsoElsovel, elsoMasodikkal, elsoT);
+            System.out.println(ki);
+            szamlalo++;
+            ki=melyikVanJoHelyen(masodikMasodikkal, masodikElsovel, masodikT);
+            System.out.println(ki+"\n");
+            tipp = beker();
+            elsoT=tipp.charAt(0);
+            masodikT=tipp.charAt(1);
         }
-
+        System.out.println("Gratulálok, sikeresen kitaláltad a szót.");
     }
 
-    private static void newGame() {
+    private static void newGame() { //csak kétszer lehetett játszani
         System.out.println("Szeretnél játstszani még egy kört? (I/N)");
         String awn = scanner.nextLine();
-        while (!(awn.equals("I") || awn.equals("N"))) {
+        while (!(awn.equals("N"))) {
+            dontes();
             System.out.println("Szeretnél játstszani még egy kört? (I/N)");
             awn = scanner.nextLine();
         }
-        if (awn.equals("I")) {
-            dontes();
-        } else {
-            System.out.println("Köszönjük hogy játszottál");
-        }
+        
+        System.out.println("Köszönjük, hogy játszottál!");
     }
 
     public static void main(String[] args) {
-        // String kitalalando = valaszt();
         dontes();
         newGame();
+    }
+
+    private static String melyikVanJoHelyen(boolean joHely, boolean nemJoHely, char karakter) {
+        String kimenet="";
+        if(joHely){
+                kimenet=String.format("'%c' betű benne van, és jó helyen.", karakter);
+            }else if (nemJoHely){
+                kimenet=String.format("'%c' betű benne van, de NEM jó helyen.", karakter);
+            }else{
+                kimenet=String.format("Nincs benne a(z) %d. karakter.", szamlalo);
+            }
+        if (szamlalo==2) {
+            szamlalo=0;
+        }
+        return kimenet;
     }
 
 }
